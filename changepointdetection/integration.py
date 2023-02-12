8a19f51ed060188dc695b4e32a3049e7bb711a16
Thomas Wimmer <wimmerth@in.tum.de>
2023-02-12 16:52:36 +0100

    Added more details on parsing and todos for the integration script

--- changepointdetection/integration.py
+++ changepointdetection/integration.py
@@ -11,31 +11,67 @@ This file is a modified version of the template file given by:
 import argparse
 import time
 
-from cpdbench_utils import load_dataset, exit_success, exit_with_error, make_param_dict
+from cpdbench_utils import load_dataset, exit_success, exit_with_error, make_param_dict, exit_with_timeout
+from multiprocessing import Process, Manager
+
+TIMEOUT = 60 * 30  # 30 minutes
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Wrapper for None-detector")
+    parser = argparse.ArgumentParser(description="Wrapper for any change point detector implemented with river.")
     parser.add_argument(
         "-i", "--input", help="path to the input data file", required=True
     )
     parser.add_argument("-o", "--output", help="path to the output file")
+    parser.add_argument("--use-timeout", action="store_true")
+
+    # TODO: How do we handle the different parameters for different algorithms? How do we parse these arbitrary parameters in a kwargs dict?
+
+    # OPTION 1: Use a config file
+    # parser.add_argument("--config", help="path to the config file for the method (in YAML format)")
+
+    # OPTION 2: A little hacky, but we can use the `nargs` argument to parse a list of parameters
+    # parser.add_argument("--parameters", nargs="+", help="list of parameters for the method")
+
+    # OPTION 3: Also hacky: use parse_known_args() to parse the parameters and then manually parse the rest
+    # parsed, unparsed = parser.parse_known_args()
+    # for arg in unparsed:
+    #     if arg.startswith(("-", "--")):
+    #       parser.add_argument(arg.split("=")[0])
+
     return parser.parse_args()
 
 
-# def main():
-#     args = parse_args()
-#
-#     data, mat = load_dataset(args.input)
-#
-#     start_time = time.time()
-#
-#     locations = []
-#
-#     stop_time = time.time()
-#     runtime = stop_time - start_time
-#
-#     exit_success(data, args, {}, locations, runtime, __file__)
+def wrap_with_timeout(args, kwargs, limit):
+    """
+    Run a change point detector with a timeout.
+
+    :param args: Arguments to pass to the change point detector.
+    :param kwargs: Keyword arguments to pass to the change point detector.
+    :param limit: Timeout in seconds.
+    :return: A tuple containing the change point detector and a string indicating the status.
+    """
+
+    def wrapper(args, return_dict, **kwargs):
+        """
+        Wrapper function to run the change point detector in a separate process.
+        """
+        detector = run_method(*args, **kwargs)  # TODO
+        return_dict["detector"] = detector
+
+    manager = Manager()
+    return_dict = manager.dict()
+
+    p = Process(target=wrapper, args=(args, return_dict), kwargs=kwargs)
+    p.start()
+    p.join(limit)
+    if p.is_alive():
+        p.terminate()
+        return None, "timeout"
+    if "detector" in return_dict:
+        return return_dict["detector"], "success"
+    return None, "fail"
+
 
 def main():
     args = parse_args()
@@ -45,8 +81,7 @@ def main():
 
     # set algorithm parameters that are not varied in the grid search
     defaults = {
-        'param_1': value_1,
-        'param_2': value_2
+        # TODO: add default parameters
     }
 
     # combine command line arguments with defaults
@@ -59,19 +94,26 @@ def main():
 
     # run the algorithm in a try/except
     try:
-        locations = your_custom_method(mat, parameters)
-        status = 'success'
+        if args.use_timeout:
+            detector, status = wrap_with_timeout((mat, parameters), kwargs, TIMEOUT)
+        else:
+            detector = run_method(mat, parameters, **kwargs)  # TODO
+            status = 'success'
     except Exception as err:
         error = repr(err)
 
     stop_time = time.time()
     runtime = stop_time - start_time
 
+    if status == 'timeout':
+        exit_with_timeout(data, args, parameters, runtime, __file__)
     # exit with error if the run failed
-    if status == 'fail':
+    if error is not None or status == 'fail':
         exit_with_error(data, args, parameters, error, __file__)
 
     # make sure locations are 0-based and integer!
+    # TODO: extract locations
+    locations = None
 
     exit_success(data, args, parameters, locations, runtime, __file__)
 
