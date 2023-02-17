c862f070bdc60d3000023ff3cd6c1170d449cff5
Jooles2001 <jules.soria@gmail.com>
2023-02-17 15:07:30 +0100

    added airline to init

--- changepoints/datasets/__init__.py
+++ changepoints/datasets/__init__.py
@@ -2,5 +2,6 @@ from . import base
 from .bitcoin import Bitcoin
 from .brent_crude_oil import BrentSpotPrice
 from .uk_coal_employment import UKCoalEmploy
+from .airline_passengers import AirlinePassengers
 
-__all__ = [base, Bitcoin, BrentSpotPrice, UKCoalEmploy]
+__all__ = [base, Bitcoin, BrentSpotPrice, UKCoalEmploy, AirlinePassengers]
