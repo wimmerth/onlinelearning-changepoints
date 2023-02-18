54f8d5b7cd6bba29520323c5cf569ed49b89eb23
Thomas Wimmer <wimmerth@in.tum.de>
2023-02-18 20:32:11 +0100

    Added a multivariate dataset

--- changepoints/datasets/__init__.py
+++ changepoints/datasets/__init__.py
@@ -3,5 +3,6 @@ from .bitcoin import Bitcoin
 from .brent_crude_oil import BrentSpotPrice
 from .uk_coal_employment import UKCoalEmploy
 from .airline_passengers import AirlinePassengers
+from .run_log import RunLog
 
-__all__ = [base, Bitcoin, BrentSpotPrice, UKCoalEmploy, AirlinePassengers]
+__all__ = ["base", "Bitcoin", "BrentSpotPrice", "UKCoalEmploy", "AirlinePassengers", "RunLog"]
--- changepoints/test.py
+++ changepoints/test.py
@@ -1,10 +1,10 @@
 from evaluation.evaluate import benchmark
 from methods import ZeroPredictor, NaiveCPD
 from metrics.changepoints import F1, Covering
-from datasets import UKCoalEmploy, BrentSpotPrice, Bitcoin, AirlinePassengers
+from datasets import *
 
 if __name__ == "__main__":
-    datasets = [UKCoalEmploy(), BrentSpotPrice(), Bitcoin(), AirlinePassengers()]
+    datasets = [RunLog()]
     metrics = F1() + Covering()
     print(benchmark(ZeroPredictor(), datasets, metrics))
     print(benchmark(NaiveCPD(lookback_window=5, alpha=0.1), datasets, metrics))
